7082b6197ef9fd14e0c85411ea7ab317ceca6f63
Vladislav Gorbatenko <vladislavtennis@outlook.com>
2022-10-22 20:39:12 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const expenses = [
@@ -25,7 +28,7 @@ const expenses = [
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
