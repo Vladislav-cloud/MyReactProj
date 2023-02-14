2ff7a76ccc3e871cd10b40e6f1cca2aad440a99d
Vladislav Gorbatenko <vladislavtennis@outlook.com>
2023-02-13 23:48:43 -0600

    push

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,25 @@
+import react from "react";
+
+import './ChartBar.css';
+
+const ChartBar = props => {
+    let barFillHeight = '0%';
+
+    if (props.max > 0) {
+        barFillHeight = math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div 
+                    className="chart-bar__fill" 
+                    style={{neight: barFillHeight}}>
+                </div>
+            </div>
+            <div className="chart-bar__label">{props.label}</div>
+        </div>
+    );
+};
+
+export default ChartBar;
